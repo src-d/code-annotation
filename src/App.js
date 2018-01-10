28760b45e68a07635fa83ffd8f1e9cbf24f95ac3
Maxim Sukharev <maxim@sourced.tech>
2018-01-10 18:55:44 +0100

    Add noop component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import logo from './logo.svg';
 import './App.css';
+import Noop from './components/Noop';
 
 class App extends Component {
   render() {
@@ -14,6 +15,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <Noop />
         <p>Test redux: {this.props.reduxValue}</p>
       </div>
     );
