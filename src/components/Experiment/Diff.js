91dbefc27fe9d5f27a8eb86d7d4d7a0e1a3e1e91
Maxim Sukharev <maxim@sourced.tech>
2018-01-15 16:54:16 +0100

    use pure component for diff
    Signed-off-by: Maxim Sukharev <maxim@sourced.tech>

--- src/components/Experiment/Diff.js
+++ src/components/Experiment/Diff.js
@@ -1,11 +1,9 @@
-import React, { Component } from 'react';
+import React, { PureComponent } from 'react';
 import { Diff2Html } from 'diff2html';
 import 'diff2html/dist/diff2html.css';
 import './Diff.less';
 
-// TODO add componentWillReceive props
-// we need to minimize rerender of this component, because it's very heavy one
-class Diff extends Component {
+class Diff extends PureComponent {
   render() {
     const { diffString, className } = this.props;
     const diffHTML = Diff2Html.getPrettyHtml(diffString, {
