583e08e7d3427b4ab1553464eb1c3c77d760db69
Maxim Sukharev <maxim@sourced.tech>
2018-01-16 17:27:36 +0100

    beautify index page
    Signed-off-by: Maxim Sukharev <maxim@sourced.tech>

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -9,16 +9,31 @@ class Index extends Component {
     const { logIn: onClick } = this.props;
     return (
       <div className="index-page">
-        <h1>Welcome! We&#39;re glad you made it this far.</h1>
-        <p>
-          source{'{d}'} code annotation brings together state-of-art insights
-          from<br />machine learning and user experience, for source code
-          annotation.
-        </p>
+        <div className="index-page__wrapper">
+          <h1 className="index-page__header">
+            Welcome! We&#39;re glad you made it this far.
+          </h1>
+          <p className="index-page__description">
+            source{'{d}'} code annotation brings together state-of-art insights
+            from<br />machine learning and user experience, for source code
+            annotation.
+          </p>
 
-        <Button onClick={onClick} bsStyle="primary">
-          SIGN IN
-        </Button>
+          <div className="index-page__actions">
+            <Button
+              onClick={onClick}
+              bsStyle="primary"
+              className="index-page__github-button"
+            >
+              Sign in with Github
+              <img src="/github.png" className="index-page__github-icon" />
+            </Button>
+          </div>
+
+          <div className="index-page__preview">
+            <img src="/assigment.png" alt="assigment" width="650" />
+          </div>
+        </div>
       </div>
     );
   }
