8a9d785577ceb5e7748b3b3599c94da2440ef5d1
Maxim Sukharev <maxim@sourced.tech>
2018-01-25 12:26:32 +0100

    fix window.localStorage in CI
    Signed-off-by: Maxim Sukharev <maxim@sourced.tech>

--- setupJest.js
+++ setupJest.js
@@ -1,1 +1,8 @@
+const { LocalStorage } = require('node-localstorage');
+
 global.fetch = require('jest-fetch-mock');
+
+global.localStorage = new LocalStorage('./localStorageTemp');
+
+global.window = document.defaultView;
+global.window.localStorage = global.localStorage;
