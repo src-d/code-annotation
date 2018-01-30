83592bfe0a0a13b54eab108a8888865fa4090c1c
David Pordomingo <David.Pordomingo.F@gmail.com>
2018-01-30 15:59:42 +0100

    Implement handlers
    From the PR feedback:
    - Expose right HTTP status codes as spotted by @smacker

--- server/handler/experiments.go
+++ server/handler/experiments.go
@@ -1,15 +1,33 @@
 package handler
 
 import (
+	"fmt"
 	"net/http"
+	"strconv"
 
+	"github.com/src-d/code-annotation/server/repository"
 	"github.com/src-d/code-annotation/server/serializer"
+
+	"github.com/go-chi/chi"
 )
 
 // GetExperimentDetails returns a function that returns a *serializer.Response
 // with the details of a requested experiment
 func GetExperimentDetails() RequestProcessFunc {
 	return func(r *http.Request) (*serializer.Response, error) {
-		return nil, serializer.NewHTTPError(http.StatusNotImplemented)
+		requestedExperimentID := chi.URLParam(r, "experimentId")
+		experimentID, err := strconv.Atoi(requestedExperimentID)
+		if err != nil {
+			return nil, serializer.NewHTTPError(
+				http.StatusBadRequest, fmt.Sprintf("wrong format in experiment ID sent; received %s", requestedExperimentID),
+			)
+		}
+
+		experiment, err := repository.GetExperimentByID(experimentID)
+		if err != nil {
+			return nil, serializer.NewHTTPError(http.StatusNotFound, "no experiment found")
+		}
+
+		return serializer.NewExperimentResponse(experiment), nil
 	}
 }
